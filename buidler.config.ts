4397e21d4b36ebc5f500b1cee1d7d38a4ce3c2c7
Taconomics <taconomics101@gmail.com>
2020-08-29 19:19:51 -0700

    Add typescript support

--- buidler.config.ts
+++ buidler.config.ts
@@ -7,7 +7,7 @@ usePlugin("buidler-typechain");
 // This object can have the following optional entries:
 // defaultNetwork, networks, solc, and paths.
 // Go to https://buidler.dev/config/ to learn more
-module.exports = {
+const config = {
   // This is a sample solc configuration that specifies which version of solc to use
   solc: {
     version: "0.6.8",
@@ -17,3 +17,5 @@ module.exports = {
     target: "ethers-v4",
   },
 };
+
+export default config;
