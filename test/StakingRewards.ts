885e696b1a23e13fbd24771e50e92a6af1e4eeb8
Taconomics <taconomics101@gmail.com>
2020-09-01 23:06:54 -0700

    now compiles

--- test/StakingRewards.ts
+++ test/StakingRewards.ts
@@ -1,10 +1,11 @@
 import { ethers } from "@nomiclabs/buidler";
 import { expect } from "chai";
-import { deployMockContract } from '@ethereum-waffle/mock-contract';
-import { Signer } from "ethers";
+import { Signer, Wallet } from "ethers";
 
 import { StakingRewardsFactory } from "../src/types/StakingRewardsFactory";
 import { StakingRewards } from "../src/types/StakingRewards";
+
+let TACO = "0x00d1793d7c3aae506257ba985b34c76aaf642557";
 
 describe("StakingRewards", function() {
   let deployer: Signer;
@@ -15,7 +16,7 @@ describe("StakingRewards", function() {
   });
 
   it("deploys contract", async function() {
-    stakingRewards = await (new StakingRewardsFactory(deployer)).deploy('a', 'b') 
+    stakingRewards = await (new StakingRewardsFactory(deployer)).deploy(TACO, TACO);
     await stakingRewards.deployed();
     
     expect(stakingRewards.address).to.not.be.null;
